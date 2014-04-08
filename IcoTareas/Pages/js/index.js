bd7d207fc4fac04daa76ad14179c76d9406dbd77
enrique248 <enrique-248@hotmail.com>
2014-04-08 17:49:19 +0200

    1
    subi nuevo proyecto

---
+++ IcoTareas/Pages/js/index.js
@@ -0,0 +1,71 @@
+﻿
+
+
+
+
+db = null;
+
+$(document).ready(function onDeviceReady() {
+    //conectar base de datos
+    db = window.openDatabase('basededatos', '1.0', 'prueba', 1000000);
+});
+
+function AgregarMaterias() {
+    $.mobile.changePage('#Materias', { transition: 'pop', role: 'dialog' });
+}
+function ConsultarMaterias() {
+    $.mobile.changePage('#ConsultaMaterias', { transition: 'pop', role: 'dialog' });
+}
+
+function onInsert()
+{
+    db.transaction(insertTask, ErrorDB);
+}
+
+function onSelectt(){
+    db.transaction(SelectTask,ErrorDB);
+}
+
+function SelectTask(tx){
+    tx.executeSql('SELECT * FROM MATERIAS',[], querySuccess, ErrorDB);
+}
+
+function querySuccess(tx,result){
+    $('#_materias').empty();
+    for (var i=0; i<result.rows.length;i++) {
+        $('#_materias').append('<li><a  href="javascript:ConsultaMaterias(' + result.rows.item(i).id + ');" data-rel="dialog">' + result.rows.item(i).materia + '</a></li>').listview('refresh');
+    }
+    }
+
+    
+
+function insertTask(tx) {
+    tx.executeSql('CREATE TABLE IF NOT EXISTS MATERIAS (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, materia)');
+
+    var _materia = $('#fMaterias').val();
+
+    tx.executeSql('INSERT INTO MATERIAS (materia) VALUES("' + _materia + '")');
+}
+
+function ErrorDB(error) {
+    alert("Error: " + error);
+}
+
+
+
+
+$(document).on("pagebeforeshow", "#ConsultaMaterias",
+
+            function () {
+                onSelectt();
+
+            });
+
+
+//Cargar pagina 
+$(document).on("pagebeforeshow", "#Materias",
+    function () {
+        $("#fMaterias").text("");
+
+    });
+  
